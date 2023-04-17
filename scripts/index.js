9a021e87a0425c0146e9162a9c2bccf77a56ebd9
Pavel Eremich <Fiadon@yandex.ru>
2023-04-17 23:53:44 +0700

    Третья попытка

--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,30 @@ let buttonEd = document.querySelector(".profile__button-edit");
 let popupEd = document.querySelector(".popup");
 let inputName = document.querySelector(".profile__name-edit");
 let inputProfession = document.querySelector(".profile__profession");
-let popupName = document.querySelector(".popup__input_name");
-let popupJob = document.querySelector(".popup__input_job");
+let popupName = document.querySelector(".popup__input_type_name");
+let popupJob = document.querySelector(".popup__input_type_job");
 let formAdd = document.querySelector(".popup__form");
 let buttonClose = document.querySelector(".popup__close")
 
 function openPopup() {
     popupEd.classList.add('popup_opened');
-    popupName.value = inputName.textContent;
-    popupJob.value = inputProfession.textContent;
 };
 
-buttonEd.addEventListener("click", openPopup);
+buttonEd.addEventListener("click", function() {
+    popupName.value = inputName.textContent;
+    popupJob.value = inputProfession.textContent;
+    openPopup();
+});
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     inputName.textContent = popupName.value;
     inputProfession.textContent = popupJob.value;
-    popupEd.classList.remove('popup_opened');
+    closePopup();
 }
 formAdd.addEventListener('submit', handleFormSubmit);
 
 function closePopup() {
-  popupEd.classList.remove('popup_opened');
+    popupEd.classList.remove('popup_opened');
 }
 buttonClose.addEventListener('click', closePopup);
